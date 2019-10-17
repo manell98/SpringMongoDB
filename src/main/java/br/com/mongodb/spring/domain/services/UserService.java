273fa1f52052be34b55279c57b8ea91f39641c16
Emanuell Santos <dfmanu06@gmail.com>
2019-10-17 16:30:56 -0300

    Obtendo um usuario por id

--- src/main/java/br/com/mongodb/spring/domain/services/UserService.java
+++ src/main/java/br/com/mongodb/spring/domain/services/UserService.java
@@ -1,11 +1,13 @@
 package br.com.mongodb.spring.domain.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.mongodb.spring.domain.User;
+import br.com.mongodb.spring.domain.services.exception.ObjectNotFoundException;
 import br.com.mongodb.spring.repositories.UserRepository;
 
 @Service
@@ -16,6 +18,13 @@ public class UserService {
 
 	public List<User> findAll() {	
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(
+								"Objeto não encontrado! Id: " + id));
 	}
 	
 }
---
+++ src/main/java/br/com/mongodb/spring/resoruces/exception/ResourceExceptionHandler.java
@@ -0,0 +1,23 @@
+package br.com.mongodb.spring.resoruces.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.mongodb.spring.domain.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest request){
+		
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), "Não encontrado", ex.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		
+	}
+	
+}
